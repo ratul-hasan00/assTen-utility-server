cd8a423b89674bdc420593884f646697a3a6b7e5
ratul-hasan00 <afif.ratul00@gmail.com>
2025-11-24 01:20:15 +0600

    second commit-added users info to database

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
-// middleWare 
-
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// BBt8131hyL7v8lJ5 
-// simpleDB 
+// MongoDB Connection URI
 const uri = "mongodb+srv://simpleDB:BBt8131hyL7v8lJ5@cluster0.5cp4gli.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
@@ -23,66 +21,105 @@ const client = new MongoClient(uri, {
 
 app.get('/', (req, res) => {
     res.send('simple crud is running');
-})
+});
 
 async function run() {
     try {
         await client.connect();
 
-        const db = client.db('smart_db')
+        const db = client.db('smart_db');
         const billsCollections = db.collection('bills');
+        const userCollections = db.collection('users');
 
-        app.get('/bills', async (req,res)=>{
+        // ----------------------- USERS RELATED -----------------------
+
+        // Create user (manual + Google register)
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log("Received user:", newUser);
+
+            // Check if user exists already
+            const exists = await userCollections.findOne({ email: newUser.email });
+            if (exists) {
+                return res.send({ message: "User already exists" });
+            }
+
+            const result = await userCollections.insertOne(newUser);
+            res.send(result);
+        });
+
+        // Update user profile (name, photo)
+        app.patch('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const updatedUser = req.body;
+
+            const filter = { email: email };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    photoURL: updatedUser.photoURL
+                }
+            };
+
+            const result = await userCollections.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        // ----------------------- BILLS CRUD -----------------------
+
+        app.get('/bills', async (req, res) => {
             const cursor = billsCollections.find();
             const result = await cursor.toArray();
             res.send(result);
-        })
+        });
 
-        app.get('/bills/:id', async (req,res)=>{
+        app.get('/bills/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) };
             const result = await billsCollections.findOne(query);
             res.send(result);
-        })
+        });
 
         app.post('/bills', async (req, res) => {
             const newBill = req.body;
             const result = await billsCollections.insertOne(newBill);
             res.send(result);
-        })
+        });
 
         app.patch('/bills/:id', async (req, res) => {
             const id = req.params.id;
-            const updatedBills = req.body;
-            const query = { _id: new ObjectId(id) }
-            const update= {
-                $set:{
-                    name: updatedBills.name,
-                    price: updatedBills.price
+            const updatedBill = req.body;
+
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: {
+                    name: updatedBill.name,
+                    price: updatedBill.price
                 }
-            }
-            const result = await billsCollections.updateOne(query,update)
+            };
+
+            const result = await billsCollections.updateOne(query, update);
             res.send(result);
-        })
+        });
 
         app.delete('/bills/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) };
             const result = await billsCollections.deleteOne(query);
-            res.send(result)
-        })
+            res.send(result);
+        });
 
+        // Successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. Connected to MongoDB!");
 
-    }
-    finally {
-        // await client.close();
+    } finally {
+        // client.close();  // keep open
     }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`simple crud is running ${port}`);
-})
+    console.log(`simple crud is running on port ${port}`);
+});
